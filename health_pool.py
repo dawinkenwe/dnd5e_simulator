76e18a566ebcb6365b3f496fc2ceb9b3cb241f27
dawinkenwe <dwinkenwerder@gmail.com>
2023-11-08 13:09:29 -0800

    Add test cases for health pool.

--- health_pool.py
+++ health_pool.py
@@ -11,26 +11,16 @@ class HealthPool:
         self.death_save_successes = 0
         self.death_save_failures = 0
 
-    def is_dead(self) -> bool:
-        return self.death_save_failures < 3
-
-    def remove_hp(self, hp_to_remove: int) -> None:
-        if self.temporary_hp > 0:
-            if self.temporary_hp > hp_to_remove:
-                self.temporary_hp -= hp_to_remove
-                return
-            else:
-                hp_to_remove -= self.temporary_hp
-                self.temporary_hp = 0
-        self.current_hp -= hp_to_remove
-
     """
     Adds hp to current HP up to max_hp. 
     Returns the overflow amount if any.
     """
-    def add_hp(self, hp_to_add: int) -> None:
+    def add_hp(self, hp_to_add: int) -> int:
         if self.is_dead():
             return
+        if self.current_hp < 1:
+            self.current_hp = 0
+            self.reset_death_saves()
         if hp_to_add + self.current_hp > self.max_hp:
             overflow = (self.max_hp - self.current_hp)
             self.current_hp = self.max_hp
@@ -40,27 +30,31 @@ class HealthPool:
         return 0
 
     def add_temporary_hp(self, hp_to_add: int) -> None:
+        if self.is_dead():
+            return
+        if self.current_hp <= 0:
+            self.reset_death_saves()
         self.temporary_hp += hp_to_add
 
-    def is_concious(self) -> bool:
-        return self.current_hp + self.temporary_hp > 0
 
-    def num_hit_dice_remaining(self) -> int:
-        return self.available_hit_dice
+    def remove_hp(self, hp_to_remove: int) -> None:
+        if not self.is_conscious():
+            self.death_save_failures = self.death_save_successes + 1 if hp_to_remove < self.max_hp else 3
+            return
 
-    def reset_hit_dice(self):
-        self.available_hit_dice = self.num_hit_dice
+        if self.temporary_hp > 0:
+            if self.temporary_hp > hp_to_remove:
+                self.temporary_hp -= hp_to_remove
+                return
+            else:
+                hp_to_remove -= self.temporary_hp
+                self.temporary_hp = 0
 
-    def roll_hit_die(self) -> int:
-        self.available_hit_dice -= 1
-        return roll_xdy(self.hit_die)
-
-    def reset_death_saves(self) -> None:
-        self.death_save_failures = 0
-        self.death_save_successes = 0
-
-    def is_stabilized(self) -> bool:
-        return self.death_save_successes >= 3
+        self.current_hp -= hp_to_remove
+        if self.current_hp < -1 * self.max_hp:
+            self.death_save_failures = 3
+        if self.current_hp < 1:
+            self.current_hp = 0
 
     def roll_death_save(self) -> None:
         roll = roll_d20()
@@ -71,3 +65,28 @@ class HealthPool:
             self.death_save_successes += 1
         else:
             self.death_save_failures += 1
+
+    def reset_death_saves(self) -> None:
+        self.death_save_failures = 0
+        self.death_save_successes = 0
+
+    def roll_hit_die(self) -> int:
+        if self.available_hit_dice < 1:
+            return 0
+        self.available_hit_dice -= 1
+        return roll_xdy(self.hit_die)
+
+    def num_hit_dice_remaining(self) -> int:
+        return self.available_hit_dice
+
+    def reset_hit_dice(self):
+        self.available_hit_dice = self.num_hit_dice
+
+    def is_stabilized(self) -> bool:
+        return self.death_save_successes >= 3
+
+    def is_dead(self) -> bool:
+        return self.death_save_failures >= 3
+
+    def is_conscious(self) -> bool:
+        return self.current_hp + self.temporary_hp > 0
